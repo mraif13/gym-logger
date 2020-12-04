db9689a70ec97e6c35dc70c70c243cba70497a95
mraif13 <raifms1200@gmail.com>
2020-12-04 15:15:27 +0400

    more fix
    Signed-off-by: mraif13 <raifms1200@gmail.com>

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -65,7 +65,7 @@ const appendContent = (x, label = "Home") => {
             <section class="card-title"> Credits </section>
 
             <section class="card-supporting-text2">
-<p class="gudmorning">V1.3 <br/>
+<p class="gudmorning">v2.0 <br/>
 Created by mraif13</p>
             </section>
 
